8c07692dc237cac90684bef58ee43a1091414356
mkalik <mbadiankouyate@gmail.com>
2023-01-30 17:09:55 -0800

    finished up with questions

--- index.js
+++ index.js
@@ -1,85 +1,126 @@
 const role = require('./lib/classPos.js');
 const ask = require('inquirer');
-// const questions = [(dd
-console.log(role);
-
-const int1 = new role.intern('jake', '123', 'jake@test.com', 'uc eureka');
-
-console.log(int1);
-
-console.log(int1.getSchool(), int1.getName(), int1.getRole(), int1.getID());
-
-const eng1 = new role.engineer('john');
-
-console.log(eng1.getRole());
-
-console.log(eng1.getName());
-//questions for the different types of workers
+var employees = [];
+//questions for various roles (manager, engineer, intern)
 manQs = [
     {
         type: 'input',
-        name: 'man',
+        name: 'Name',
         message: "Enter your team manager's name",
     },
     {
         type: 'input',
-        name: 'manID',
+        name: 'ID',
         message: 'Enter you team managers Employee ID',
     },
     {
         type: 'input',
-        name: 'manEmail',
+        name: 'Email',
         message: "Enter your team manager's email",
     },
     {
         type: 'input',
-        name: 'manOffice',
+        name: 'Office',
         message: "Enter your team manager's office number",
     },
 ];
 engQs = [
     {
         type: 'input',
-        name: 'engName',
+        name: 'Name',
         message: "Enter your engineer's name",
     },
     {
         type: 'input',
-        name: 'engID',
+        name: 'ID',
         message: "Enter your enigneer's id number",
     },
     {
         type: 'input',
-        name: 'engEmail',
+        name: 'Email',
         message: "Enter your engineer's email",
     },
     {
         type: 'input',
-        name: 'engGithub',
+        name: 'Github',
         message: "Enter your engineer's github account name",
     },
 ];
 intQs = [
     {
         type: 'input',
-        name: 'intName',
+        name: 'Name',
         message: "Enter your intern's name",
     },
     {
         type: 'input',
-        name: 'intId',
+        name: 'ID',
         message: "Enter your intern's id number",
     },
     {
         type: 'input',
-        name: 'intEmail',
+        name: 'Email',
         message: "Enter your intern's email",
     },
     {
         type: 'input',
-        name: 'intEdu',
+        name: 'Edu',
         message: 'Enter the school your intern attends',
     },
 ];
-
-function askQuestions() {}
+//menu question that asks a user what new role they would like to add
+menuQs = [
+    {
+        type: 'confirm',
+        name: 'add',
+        message: 'Would you like to add another team member?',
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What type of member would you like to add?',
+        choices: ['Engineer', 'Intern'],
+        when: (answers) => answers.add == true,
+    },
+];
+//starts the program and immediately prompts for a team manager
+async function init() {
+    var newHead = await ask.prompt(manQs);
+    var teamHead = new role.manager(
+        newHead.Name,
+        newHead.ID,
+        newHead.Email,
+        newHead.Office
+    );
+    employees.push(teamHead);
+    askQuestions();
+}
+//adds an intern to the employees array
+async function addInt() {
+    console.log('adding an intern');
+    var int = await ask.prompt(intQs);
+    var newInt = new role.intern(int.Name, int.ID, int.Email, int.Edu);
+    employees.push(newInt);
+    askQuestions();
+}
+//adds an engineer to the employees array
+async function addEng() {
+    console.log('adding an engineer');
+    var eng = await ask.prompt(engQs);
+    var newEng = new role.engineer(eng.Name, eng.ID, eng.Email, eng.Github);
+    employees.push(newEng);
+    askQuestions();
+}
+//function that continues to prompt a user with questions until they decide not
+//to add another team member
+async function askQuestions() {
+    // console.log(employees);
+    var newMember = await ask.prompt(menuQs);
+    if (newMember.add) {
+        newMember.role == 'Engineer' ? addEng() : addInt();
+    } else {
+        console.log(employees);
+        console.log('goodbye');
+    }
+}
+init();
